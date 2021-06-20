9a2fc62ccb380870292649a88ea1ea7e9873d033
Marsha Hall <mphall77@gmail.com>
2021-06-20 15:35:03 -0400

    add  routes to the backend

--- app.js
+++ app.js
@@ -12,7 +12,6 @@ app.use(cors());
 app.use(express.json());
 app.use(cors());
 app.use((req, res, next) => {
-	console.log(`testing 101`);
 	next();
 });
 
--- controllers/transactionsController.js
+++ controllers/transactionsController.js
@@ -11,12 +11,39 @@ transactions.get("/", (req, res) => {
 });
 
 // SHOW ROUTE AND REDIRECT
+transactions.get("/:index", (req, res) => {
+	const { index } = req.params;
+	const transaction = transactionsArray[index];
+	if (transaction) {
+		res.json(transaction);
+	} else {
+		res.redirect(`404`);
+	}
+});
 
 // CREATE ==> POST request to /bookmarks
+transactions.post("/", (req, res) => {
+	const { body } = req;
+	transactionsArray.push(body);
+	const newIdx = transactionsArray.length - 1;
+	res.json(transactionsArray[newIdx]);
+	// res.json(transactionsArray); all of the array
+});
 
 // PUT
+transactions.put("/:index", (req, res) => {
+	const { index } = req.params;
+	const { body } = req;
+	transactionsArray[index] = body;
+	res.json(transactionsArray[index]);
+});
 
 // DELETE
+transactions.delete("/:index", (req, res) => {
+	const { index } = req.params;
+	const deletedTransaction = transactionsArray.splice(index, 1);
+	res.json(deletedTransaction[0]);
+});
 
 // EXPORT
 module.exports = transactions;
